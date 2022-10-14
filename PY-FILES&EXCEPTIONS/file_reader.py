07caab37e238ea86d5f46538ae2c629180c9accf
SamedWalker <samedwalker50@gmail.com>
2022-10-14 09:53:34 -0400

    Modified initial commit

--- PY-FILES&EXCEPTIONS/file_reader.py
+++ PY-FILES&EXCEPTIONS/file_reader.py
@@ -1,5 +1,8 @@
 # Sometimes your file would be in a folder file path
 # So in that case you provide a file path like file_path/pi_digits.txt
+from turtle import title
+
+
 with open('text_files\pi_digits.txt') as file_object:
   contents = file_object.read()
 # print(contents.rstrip())
@@ -68,27 +71,93 @@ with open(filename) as file_object:
 
 # EXCEPTIONS:
 # USING TRY-EXCEPT BLOCKS
-try:
-  print(5/0)
-except ZeroDivisionError:
-  print("You can't divide by zero!")
+# try:
+#   print(5/0)
+# except ZeroDivisionError:
+#   print("You can't divide by zero!")
 
-# USING EXCEPTIONS TO PREVENT CRASHES
-# The else Block
-# division_calculator.py
-print("Give me two numbers, and I'll divide them.")
-print("Enter 'q' to quit.")
+# # USING EXCEPTIONS TO PREVENT CRASHES
+# # The else Block
+# # division_calculator.py
+# print("Give me two numbers, and I'll divide them.")
+# print("Enter 'q' to quit.")
 
-while True:
-  first_number = input("\nFirst number: ")
-  if first_number == 'q':
-    break
-  second_number = input("Second number: ")
-  if second_number == 'q':
-    break
+# while True:
+#   first_number = input("\nFirst number: ")
+#   if first_number == 'q':
+#     break
+#   second_number = input("Second number: ")
+#   if second_number == 'q':
+#     break
+#   try:
+#     answer = int(first_number) / int(second_number)
+#   except ZeroDivisionError:
+#     print("You can't divide by 0!")
+#   else:
+#     print(answer)
+
+# Handling the FileNotFoundError Exception
+"""Trying to read a file that doesn't exist in the file.
+   The open() function produced the error, so to handle it, 
+   we use the try block
+"""
+# filename = 'text_files/alice.txt'
+
+# try:
+#   with open(filename, encoding='utf-8') as f:
+#     contents = f.read()
+# except FileNotFoundError:
+#   print(f"Sorry, the file {filename} does not exist.")
+# else:
+#   # Count the approximate number of words in the file.
+#   words = contents.split()
+#   num_words = len(words)
+#   print(f"The file {filename} has about {num_words} words.")
+
+
+# ANALYZING TEXT
+# title = "Alice in Wonderland"
+# now = title.split()
+# print(now)
+
+# WORKING WITH MULTIPLE FILES
+"""Working with multiple books would require the
+  use of a function to make our code compact. 
+"""
+# def count_words(filename):
+#   """Count the approximate number of words in a file."""
+#   try:
+#     with open(filename, encoding='utf-8') as f:
+#       contents = f.read()
+#   except FileNotFoundError:
+#     print(f"Sorry, the file {filename} does not exist.")
+#   else:
+#   # Count the approximate number of words in the file.
+#     words = contents.split()
+#     num_words = len(words)
+#     print(f"The file {filename} has about {num_words} words.")
+# filenames = ['text_files/alice.txt', 'text_files/siddhartha.txt', 'text_files/moby_dick.txt', 'text_files/little_women.txt']
+# for filename in filenames:
+#   count_words(filename)
+
+# FAILING SILENTLY
+def count_words(filename):
+  """Count the approximate number of words in a file."""
   try:
-    answer = int(first_number) / int(second_number)
-  except ZeroDivisionError:
-    print("You can't divide by 0!")
+    with open(filename, encoding='utf-8') as f:
+      contents = f.read()
+  except FileNotFoundError:
+    """To make a code fail silently python uses the pass statement.
+    The pass statement acts as a placeholder.
+    """
+    pass
   else:
-    print(answer)
+  # Count the approximate number of words in the file.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {filename} has about {num_words} words.")
+filenames = ['text_files/alice.txt', 'text_files/siddhartha.txt', 'text_files/moby_dick.txt', 'text_files/little_women.txt']
+for filename in filenames:
+  count_words(filename)
+
+# DECIDING WHICH ERRORS TO REPORT
