54610bb4de78f6a13a5d3fd53fdbcf82b5346ce3
SamedWalker <samedwalker50@gmail.com>
2022-10-24 12:19:00 -0400

    Modified the initial commit

---
+++ python_works/favorite_number.py
@@ -0,0 +1,4 @@
+# Calling out my favorite number in a message!
+favorite_number = 15
+message = f"My favorite number is {favorite_number}."
+print(message)
---
+++ python_works/number_eight.py
@@ -0,0 +1,6 @@
+# Printing out the outcome of two numbers 
+# which results in a value of 8.
+print(5+3)
+print(15-7)
+print(4*2)
+print(16/2)
