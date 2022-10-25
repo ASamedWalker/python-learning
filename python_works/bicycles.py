10c8a7e9954d1a2161b9c1b67b7bb7fc871c667f
SamedWalker <samedwalker50@gmail.com>
2022-10-24 21:51:34 -0400

    Modified initial commit

---
+++ python_works/bicycles.py
@@ -0,0 +1,20 @@
+bicycles = ['trek', 'cannondale', 'readline', 'specialized']
+# printing out the collection of elements in a list
+# print(bicycles)
+
+# printing out an item or elements(index position) from the list
+# print(bicycles[0])
+
+# printing a formatted item from the list using a string method
+# print(bicycles[0].title())
+
+# printing index positions starting at 0, not 1.
+# print(bicycles[1])
+# print(bicycles[3])
+
+# printing the last index in the list
+# print(bicycles[-1])
+
+# printing out a message out the individual items in the list
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
---
+++ python_works/name_friends.py
@@ -0,0 +1,11 @@
+# Storing a collection of friends name in a list
+from unicodedata import name
+
+
+name_friends = ['bob', 'jake', 'doe', 'sam']
+
+# Printing each person's name from the list
+print(name_friends[0].title())
+print(name_friends[1].title())
+print(name_friends[2].title())
+print(name_friends[3].title())
